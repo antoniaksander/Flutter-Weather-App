a68717ef279ad3a861c5061ab6acb76db03c3148
antoniaksander <36177687+antoniaksander@users.noreply.github.com>
2019-10-03 13:38:18 +0300

    Add files via upload

---
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,22 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+#import <geolocator/GeolocatorPlugin.h>
+#import <google_api_availability/GoogleApiAvailabilityPlugin.h>
+#import <location_permissions/LocationPermissionsPlugin.h>
+#import <permission_handler/PermissionHandlerPlugin.h>
+#import <shared_preferences/SharedPreferencesPlugin.h>
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [GeolocatorPlugin registerWithRegistrar:[registry registrarForPlugin:@"GeolocatorPlugin"]];
+  [GoogleApiAvailabilityPlugin registerWithRegistrar:[registry registrarForPlugin:@"GoogleApiAvailabilityPlugin"]];
+  [LocationPermissionsPlugin registerWithRegistrar:[registry registrarForPlugin:@"LocationPermissionsPlugin"]];
+  [PermissionHandlerPlugin registerWithRegistrar:[registry registrarForPlugin:@"PermissionHandlerPlugin"]];
+  [FLTSharedPreferencesPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTSharedPreferencesPlugin"]];
+}
+
+@end
